367228c8c91153057f2519b95184bc29731ead89
Andrew Lee <lichundian@gmail.com>
2019-06-16 16:49:59 +0800

    Add bold log.

--- run.sh
+++ run.sh
@@ -2,15 +2,19 @@
 username=xxx
 passwd=xxx
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
 while true; do
+    echo "${bold}Connecting... Hold on for a second.${normal}"
     ping -c 1 www.baidu.com 1>/dev/null 2>&1
     if [[ $? -ne 0 ]]; then
-        echo "Retry login as user:$username and password:$passwd"
+        echo "${bold}Retry login as user:${username} and password:${passwd}${normal}"
         python ict_dial.py $username $passwd
     else
-      echo "Connected"
+      echo "${bold}Connected${normal}"
     fi
-    echo "Wait 1 hour to beat heart."
-    echo "......"
+    echo "${bold}Wait 1 hour to beat heart."
+    echo "......${normal}"
     sleep 3600
 done
